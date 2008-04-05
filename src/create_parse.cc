df00c1aad9fee851210a705eb753fca013682bde
christof.petig <christof.petig@arcor.de>
2008-04-05 21:25:37 +0000

    fix configuration to use itos

--- src/create_parse.cc
+++ src/create_parse.cc
@@ -17,6 +17,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <ManuProCConfig.h>
 #include <Misc/create_parse.h>
 #include <iostream>
 #include <locale.h>
