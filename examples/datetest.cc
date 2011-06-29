8e6404ad1065d873e4d7b15e688d85b634f28be7
petig <petig@stahlwille.de>
2011-06-29 12:40:06 +0000

    make sure that it was not create_parse's fault

--- examples/datetest.cc
+++ examples/datetest.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <Misc/Datum.h>
+#include <Misc/create_parse.h>
 
 int main()
 {
   std::cout << ManuProC::Datum::from_access("14.04.2012 00:00:00") << '\n';
+  std::cout << ManuProC::parse<float>("-24.8") << '\n';
   return 0;
 }
