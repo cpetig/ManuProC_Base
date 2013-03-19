d4df4b0d37f1c7cf2145e3d3d6f2165bc9c7c5ba
jacek <jacek@telebel.de>
2013-03-19 10:41:40 +0000

    easy config.h for VS2010

--- config_static.h
+++ config_static.h
@@ -19,124 +19,12 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#define DEFAULT_DB "jacek"
+#define DEFAULT_DB "stw.db"
 #define DEFAULT_DBHOST ""
 
 /* whether handle content tracing is enabled */
 /* #undef DEBUG_HANDLE_CONTENT */
 
-/* Define to 1 if translation of program messages to the user's native
-   language is requested. */
-#define ENABLE_NLS 1
-
-/* the gettext domain */
-#define GETTEXT_PACKAGE "ManuProC_Base"
-
-/* Define to 1 if you have the `bind_textdomain_codeset' function. */
-#define HAVE_BIND_TEXTDOMAIN_CODESET 1
-
-/* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
-   CoreFoundation framework. */
-/* #undef HAVE_CFLOCALECOPYCURRENT */
-
-/* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
-   the CoreFoundation framework. */
-/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
-
-/* Define if the GNU dcgettext() function is already present or preinstalled.
-   */
-#define HAVE_DCGETTEXT 1
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-/* #undef HAVE_DLFCN_H */
-
-/* Define if the GNU gettext() function is already present or preinstalled. */
-#define HAVE_GETTEXT 1
-
-/* Define if you have the iconv() function and it works. */
-/* #undef HAVE_ICONV */
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define if your <locale.h> file defines LC_MESSAGES. */
-/* #undef HAVE_LC_MESSAGES */
-
-/* Define to 1 if using 8.3+ ecpg (no ECPGdescriptor_lvalue). */
-/* #undef HAVE_LIBECPG6_UP */
-
-/* Define to 1 if you have the <locale.h> header file. */
-#define HAVE_LOCALE_H 1
-
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if PQprepare is available in libpq. */
-/* #undef HAVE_PQPREPARE */
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#define LT_OBJDIR ".libs/"
-
-/* a database is present */
-#define MANUPROC_WITH_DATABASE 1
-
-/* which SigC++ version to use (e.g. 0x220) */
-#define MPC_SIGC_VERSION 0x220
-
 /* Define to 1 if you want to use the SQLite backend */
 #define MPC_SQLITE 1
 
-/* Define to 1 if you use ecpg 2.9. */
-/* #undef OLD_ECPG */
-
-/* Name of package */
-#define PACKAGE "manuproc_base"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "manuproc-developer-de@berlios.de"
-
-/* the location of locale files */
-#define PACKAGE_LOCALE_DIR "/usr/i586-mingw32msvc/share/locale"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "ManuProC_Base"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ManuProC_Base 0.8.0"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "manuproc_base"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "0.8.0"
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
-/* Version number of package */
-#define VERSION "0.8.0"
