4fec2d58f41f2cfa2bb9a284197c9d5bcc291dd8
petig <petig@stahlwille.de>
2016-09-11 22:55:16 +0000

    mostly working sqlite3

--- src/dbconnect_sqlite3.cc
+++ src/dbconnect_sqlite3.cc
@@ -6,6 +6,7 @@
 #ifdef MPC_SQLITE
 #include <sqlite3.h>
 #include <Misc/Query.h>
+#include <string.h>
 
 struct sqliteConnection : ManuProC::Connection_base
 {
@@ -96,9 +97,80 @@ void sqliteConnection::execute(char const* query) throw(SQLerror)
 	   eof=!lines; */
 }
 
-ManuProC::Query_result_base* sqliteConnection::execute2(char const* s) throw(SQLerror)
+struct rowSQ : ManuProC::Query_result_row
 {
-	throw SQLerror(s, 100, "not implemented");
+	char **local_result;
+	int rows,cols;
+	unsigned row;
+
+	virtual unsigned columns() const throw() { return cols; }
+	virtual int indicator(unsigned col) const { return local_result[row*cols+col]!=0; }
+	virtual char const* text(unsigned col) const { return local_result[row*cols+col]; }
+};
+
+struct resultsSQ : ManuProC::Query_result_base
+{
+	sqliteConnection *conn;
+	char **local_result;
+	unsigned next_row;
+	int rows,cols;
+	int lines;
+	rowSQ rowres;
+
+	virtual unsigned LinesAffected() const throw() { return lines; }
+	virtual ManuProC::Query_result_row* Fetch()
+	{
+		if (next_row>=rows)
+		{
+			conn->last_code=100;
+			return 0;
+		}
+		rowres.local_result=local_result;
+		rowres.row=next_row;
+		rowres.rows=rows;
+		rowres.cols=cols;
+		++next_row;
+		return &rowres;
+	}
+	virtual void AddParameter(const std::string &s, ManuProC::Oid type) {}
+//	   virtual void AddParameter(long integer, Oid type)=0;
+//	   virtual void AddParameter(double fl, Oid type)=0;
+	virtual bool complete() const throw() { return true; }
+
+	resultsSQ() : conn(), local_result(), next_row(), rows(),cols(),lines() {}
+	~resultsSQ() { if (local_result) sqlite3_free_table(local_result); }
+};
+
+ManuProC::Query_result_base* sqliteConnection::execute2(char const* query) throw(SQLerror)
+{
+	char **local_result=0;
+	   char *msgbuf=0;
+	   int rows,cols;
+
+	   if (Query::debugging.on) std::cerr << "QUERY: " << query << '\n';
+
+	   int error=sqlite3_get_table(db_connection, query,
+	   		&local_result, &rows, &cols, &msgbuf);
+	   last_code=error;
+	   if (Query::debugging.on)
+	      std::cerr << "RESULT: " << error << ':' << (msgbuf?msgbuf:"")
+	      		<< ", " << rows << 'x' << cols << '\n';
+	   if(error!=SQLITE_OK)
+	   {  std::string err=msgbuf;
+	      sqlite3_free(msgbuf);
+	      throw SQLerror(query,error,err);
+	   }
+	   resultsSQ* res= new resultsSQ;
+	   res->rows= rows;
+	   res->cols= cols;
+	   res->local_result= local_result;
+	   res->lines= rows;
+	   if (msgbuf) sqlite3_free(msgbuf);
+	   if (!res->lines && strncasecmp(query,"select",6))
+	     res->lines=sqlite3_changes(db_connection);
+	   if (!res->lines) last_code=error=100;
+	   res->conn=this;
+	   return res;
 }
 
 #else
