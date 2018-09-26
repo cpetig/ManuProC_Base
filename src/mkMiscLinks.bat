07a50402cf47878f87b0f9986728196c665abd84
Jacek Jakubowski <jacek@tuxsolutions.de>
2018-09-26 19:57:52 +0200

    make links script expanded to link hpp files too

--- src/mkMiscLinks.bat
+++ src/mkMiscLinks.bat
@@ -3,16 +3,17 @@ mkdir Misc
 cd Misc
 
 ECHO > FILESINLOOP.txt
-FOR /F "USEBACKQ DELIMS=|" %%G IN (`dir /B /A:-D "..\*.h"`) DO (
+FOR /F "USEBACKQ DELIMS=|" %%G IN (`dir /B /A:-D "..\*.h" "..\*.hpp"`) DO (
 	ECHO %%G >> FILESINLOOP.txt
 	MKLINK "%%G" "..\%%G"
 )
+cd ..
 
 mkdir BaseObjects
 cd BaseObjects
 
 ECHO > FILESINLOOP.txt
-FOR /F "USEBACKQ DELIMS=|" %%G IN (`dir /B /A:-D "..\*.h"`) DO (
+FOR /F "USEBACKQ DELIMS=|" %%G IN (`dir /B /A:-D "..\*.h" "..\*.hpp"`) DO (
 	ECHO %%G >> FILESINLOOP.txt
 	MKLINK "%%G" "..\%%G"
 )
